bcd3ac7b4890d08b699aa16777637e22f825ab33
Fernando Tadeo <fernandotadeos@gmail.com>
2024-09-15 12:31:07 -0300

    proyects route created

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,11 @@
-import { Counter } from "@/components/counter";
+
 import { title } from "@/components/primitives";
 
 export default function AboutPage() {
 	return (
 		<div>
 			<h1 className={title()}>Acerca de mi </h1>
-			<Counter/>
+			<p className="text-lg hover:bg-green-900"> Hola! Soy desarrollador Fullstack al termino de mi carrera de Tec. en Desarrollo Web y Aplicaciones mobiles del ISPC </p>
 		</div>
 	);
 }
---
+++ app/proyects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const proyectsPage = () => {
+  return (
+    <div>proyectsP</div>
+  )
+}
+
+export default proyectsPage
